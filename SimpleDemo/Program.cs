8500986c2f4cd82464268908878a3d4eab3c9824
DhananjayGaikwad1 <113286839+DhananjayGaikwad1@users.noreply.github.com>
2022-10-26 10:23:15 +0530

    testDone

---
+++ SimpleDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 100;
+            int c = a + b;
+            Console.WriteLine("aaddition two number "+c);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
